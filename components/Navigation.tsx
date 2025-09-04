f78b320efa7e3627a7bf884954803258aeec6415
Md. Nazim Ahmed <nazimahmedprovat@gmail.com>
2025-09-04 14:48:47 +0600

    change text

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -71,7 +71,7 @@ export default function Navigation() {
               onClick={scrollToHome}
               className="text-xl sm:text-2xl font-bold bg-gradient-to-r from-blue-400 to-purple-400 bg-clip-text text-transparent hover:from-blue-300 hover:to-purple-300 transition-all duration-200 cursor-pointer"
             >
-              Nazim Ahmed
+              Md. Nazim Ahmed
             </button>
           </div>
 
